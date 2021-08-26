ac57e09cc332fabe4fafc763fbc629035e6fbf73
NTsar <n.tsar87@gmail.com>
2021-08-26 18:03:38 +0300

    Add words counter

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,5 +8,6 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reverse_text = user_text[::-1]
+	count_words = len(user_text.split())
 	return render(request, 'reverse.html', {'usertext':user_text,
-	 'reversedtext': reverse_text})	
+	 'reversedtext': reverse_text, 'countwords': count_words})	
